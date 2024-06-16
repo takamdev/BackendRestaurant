f8ec85ff0fd4f28fe0329cf6df4714a19df428d3
takamdev <panguslariss@gmail.com>
2024-06-16 09:39:16 +0100

    modif

--- src/routes/deleteCommt.js
+++ src/routes/deleteCommt.js
@@ -1,6 +1,6 @@
 const {deletCommt} = require("./../controller/controlers.js")
 const auth = require('./../auth/auth.js')
 module.exports = (app)=>{
-      app.post('/api/comment/delete/:id', auth ,deletCommt)
+      app.post('/api/comment/delete/:id', auth ,deletCommt) 
 }
 
